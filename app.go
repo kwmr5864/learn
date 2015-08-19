a69d195bd7891cc6a2653cea2b6195348853c7fb
Masayuki Kawamura <kwmr5864@gmail.com>
2015-08-19 22:49:38 +0900

    何も表示されないのでハンドラ登録

--- app.go
+++ app.go
@@ -1,8 +1,14 @@
 package main
 import (
 	"net/http"
+	"fmt"
 )
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは, %q", r.URL.Path[1:])
+}
+
 func main() {
+	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
